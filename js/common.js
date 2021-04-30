c2b7ac273b2de715ddf3e91dd0bd4b8c631bb08e
walkinded <labunskij1997@gmail.com>
2021-04-30 18:22:41 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
-    $(".menuToggle").click(function() {
-      $(this).toggleClass("active");
-      $('.menu').slideToggle(300, function(){
-        if($(this).css('display') === "none"){
-          $(this).removeAttr('style');
-        }
-      });
+
+  $(".menuToggle").click(function() {
+    $(this).toggleClass("active");
+    $('.menu').slideToggle(300, function(){
+      if($(this).css('display') === "none"){
+        $(this).removeAttr('style');
+      }
     });
   });
+
+
+  // initialization Swiper
+  var swiper = new Swiper('.swiper-container', {
+    speed: 800,
+    autoHeight: true,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+});
+
