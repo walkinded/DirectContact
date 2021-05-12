eee317e1d0474d920a027cf0f3c291a4ff865d41
walkinded <labunskij1997@gmail.com>
2021-05-12 10:42:36 +0300

    12.05 update header, fix
    fix "column_vertical"
    added "column_title"

--- js/common.js
+++ js/common.js
@@ -47,6 +47,13 @@ $(document).ready(function() {
   });
   
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100){
+        $('header').addClass('scrolled');         
+    }else{
+        $('header').removeClass("scrolled");         }
+  });
+  
 });
 
 
